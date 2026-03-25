bbe4887ed820a87ca365577d0eca862ec70cf4cc
Oskari <oskari.honkala@aalto.fi>
2026-03-25 12:39:02 +0200

    Fix loading function

--- src/preprocessing/linearize.py
+++ src/preprocessing/linearize.py
@@ -3,7 +3,7 @@ import numpy as np
 from PIL import Image
 
 def srgb_to_linear(img: Image.Image) -> np.ndarray:
-    rgb = np.array(img).astype(np.float32) / 255.0
+    rgb = np.array(img).astype(np.float64) / 255.0
     linear = np.where(
         rgb <= 0.04045,
         rgb / 12.92,
@@ -11,7 +11,8 @@ def srgb_to_linear(img: Image.Image) -> np.ndarray:
     )
     return linear
 
-def load(filepath: str) -> (np.ndarray, np.ndarray):
+def load(filepath: str) -> tuple[np.ndarray, np.ndarray]:
     img = Image.open(filepath).convert("RGB")
     srgb = np.array(img) / 255.0
     linear = srgb_to_linear(img)
+    return linear, srgb
